9daabcb0ba0e95fef5193e354a38db225c6c1c7f
Carol Wu <carolwuq@gmail.com>
2022-01-14 15:46:06 -0500

    second

--- bash.js
+++ bash.js
@@ -1,14 +1,20 @@
+const { pwd } = require("./pwd");
+const { ls } = require("./ls");
+
+
 //Output a prompt
 process.stdout.write("prompt > ");
 
 //The stdin 'data' event fires after a user types in a line
 process.stdin.on("data", (data) => {
   const cmd = data.toString().trim();
-  if (cmd === "pwd") {
-    process.stdout.write(process.cwd());
-    process.stdout.write("\nprompt > ");
+  if (cmd === "pwd") { 
+    pwd()
+  } else if (cmd === "ls") { 
+    ls()
   } else {
     process.stdout.write("You typed: " + cmd);
     process.stdout.write("\nprompt > ");
   }
 });
+
